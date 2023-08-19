70ade5127b4bbd6b67b29d6495cecb5af2ec3f92
tomatojams <tomatojams@naver.com>
2023-08-19 11:13:34 +0900

    n

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,13 +1,9 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-]
+const images = ["0.jpg", "1.jpg", "2.jpg"];
 
-RANDOM_IMAGE_NUMBER = Math.floor(Math.random() * images.length);
+const RANDOM_IMAGE_NUMBER = Math.floor(Math.random() * images.length);
 const chosenImage = images[RANDOM_IMAGE_NUMBER];
 
 const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`
+bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,18 +1,17 @@
-const clock = document.querySelector('h2#clock');
+const clock = document.querySelector("h2#clock");
 
-function getClock(){
-    const date = new Date();
-    const day = date.getDate();
-    const month = (String(date.getMonth())).padStart(2, '0');
-    const year = date.getFullYear();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
-    const nowTime = `${hour}:${minute}:${second}`;
+function getClock() {
+  const date = new Date();
+  // const day = date.getDate();
+  // const month = String(date.getMonth()).padStart(2, "0");
+  // const year = date.getFullYear();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const nowTime = `${hour}:${minute}:${second}`;
 
-    clock.innerText = nowTime;
+  clock.innerText = nowTime;
 }
 
-
 getClock();
-setInterval(getClock,1000)
+setInterval(getClock, 1000);

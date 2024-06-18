024a856d207adde3a7ff4b760b6f8fec580c99e3
tomatojams <tomatojams@naver.com>
2024-06-18 18:18:58 +0900

    svelte

---
+++ Svelte/Svelte_ex2/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  let message;
+
+  fetch("http://127.0.0.1:8000/state").then((response) => {
+    response.json().then((json) => {
+      message = json.state;
+    });
+  });
+</script>
+
+<h1>{message}</h1>

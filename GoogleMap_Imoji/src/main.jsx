956d33c00b97ad2b77f02d874226f7429ba20133
tomatojams <tomatojams@naver.com>
2024-12-05 22:12:00 +0900

    선택금지

--- GoogleMap_Imoji/src/main.jsx
+++ GoogleMap_Imoji/src/main.jsx
@@ -1,8 +1,19 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    user-select: none; /* 모든 요소 선택 금지 */
+  }
+`;
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+    <App />
+  </>
   // </React.StrictMode>,
 );

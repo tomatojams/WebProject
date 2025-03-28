9376e2de87a1c5876ec7636a8986cdde82e607c2
tomatojams <tomatojams@naver.com>
2025-03-29 00:35:47 +0900

    ES6 5강강

--- ES6/04- 퀴즈q1-2.js
+++ ES6/04- 퀴즈q1-2.js
@@ -3,9 +3,9 @@
 /* 1. 간단한 메소드 만들기
 
   */
-var man = {
+let man = {
   name: "토마토",
-
+  // arrow function 사용시 this를 사용할 수 없음.
   sayHi: function () {
     console.log(`안녕 나는 ${this.name}이야`);
   },
@@ -18,14 +18,18 @@ man.sayHi();
 /* 2. 오브젝트 내의 데이터를 전부 더해주는 메소드만들기  */
 
 
-var data = {
+const dataSet = {
   data: [1, 2, 3, 4, 5],
+
+  sum: function () {
+    let total = 0;
+    this.data.forEach((a) => {
+      total = total + a;
+    });
+    return total;
+   }
 };
 
-let total = 0;
-data.data.forEach((a) => {
-  total = total + a;
-});
-console.log(total);
+console.log(dataSet.sum()); // 15
 
 //Q3
